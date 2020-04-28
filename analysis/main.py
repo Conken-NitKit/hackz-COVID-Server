c310314f2b3be50959a148fa0f0a166defa1f85b
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-04-29 00:29:24 +0900

    Add: 外部からの入力に対応 #1

--- analysis/main.py
+++ analysis/main.py
@@ -2,22 +2,26 @@ import MeCab
 import itertools
 import collections
 import os
+import sys
 
 def debug(content):
     print(type(content), content)
 
 class Analysis():
-    def __init__(self, dir='./dic/mecab-ipadic-neologd'):
+    def __init__(self, dir=u'./dic/mecab-ipadic-neologd'):
         # 辞書を選択
-        self.mecab = MeCab.Tagger('-d' + os.path.join(os.path.dirname(__file__), dir))
+        self.mecab = MeCab.Tagger(u'-d' + os.path.join(os.path.dirname(__file__), dir))
 
     def main(self, text_list):
         # 単語に分解
         words = list(map(self.slice_text, text_list))
+        # print(words)
         # 二次元配列を平坦にする
         words = list(itertools.chain.from_iterable(words))
+        # print(words)
         # 単語の出現回数でソート
         most_words = self.sort_most_word(words)
+        # print(most_words)
 
         return most_words
 
@@ -42,7 +46,8 @@ class Analysis():
         return most_array
 
 if __name__ == '__main__':
-    sample_text_list = ['庭には庭庭がいる', '齋藤飛鳥は俺の齋藤飛鳥', '俺の嫁！俺の嫁！']
+    # sample_text_list = ['庭には庭庭がいる', '齋藤飛鳥は俺の齋藤飛鳥', '俺の嫁！俺の嫁！']
+    sample_text_list = sys.argv[1:]
 
     analysis = Analysis()
     result = analysis.main(sample_text_list)
